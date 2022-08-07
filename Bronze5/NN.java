6588ecd2683d4edb6bb6d15eff9fb8eb970d87a8
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2022-08-07 16:42:36 +0900

    Add files via upload

---
+++ Bronze5/NN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NN {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int sum;
+
+        for(int i=1;i<=A;i++){
+            System.out.println(i);
+        }
+    }
+}
