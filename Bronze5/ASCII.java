259154f17998b93d87a2b20998c81c3920ce1fef
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2022-08-13 17:40:33 +0900

    Add files via upload

---
+++ Bronze5/ASCII.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String st = scan.next();
+        int result = st.charAt(0);
+
+        System.out.println(result);
+    }
+}
